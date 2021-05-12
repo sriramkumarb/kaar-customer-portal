4126462bcb9578870ef61c75a3c748b436eb0d17
Sriram <sriram9159541445@gmail.com>
2021-05-12 13:04:38 +0530

    feat : register page validations updated

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,10 +9,49 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
+  myForm: FormGroup;
+  successMessage: String = '';
+
   constructor(private router: Router,
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute) {
+    this.myForm = new FormGroup({
+      email: new FormControl(null, Validators.email),
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+      cnfpass: new FormControl(null, this.passValidator)
+    });
+
+
+    this.myForm.controls.password.valueChanges
+      .subscribe(
+        x => this.myForm.controls.cnfpass.updateValueAndValidity()
+      );
+
+  }
 
   ngOnInit(): void {
+  }
+
+  isValid(controlName: any) {
+    return this.myForm.get(controlName)?.invalid && this.myForm.get(controlName)?.touched;
+  }
+
+  passValidator(control: AbstractControl) {
+    if (control && (control.value !== null || control.value !== undefined)) {
+      const cnfpassValue = control.value;
+
+      const passControl = control.root.get('password');
+      if (passControl) {
+        const passValue = passControl.value;
+        if (passValue !== cnfpassValue || passValue === '') {
+          return {
+            isError: true
+          };
+        }
+      }
+    }
+
+    return null;
   }
 
   movetologin() {
