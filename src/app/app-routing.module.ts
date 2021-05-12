e06a6ef7d5520468fef2a86953efacdfbe7a655b
Sriram <sriram9159541445@gmail.com>
2021-05-12 11:44:52 +0530

    feat : register page updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,25 @@ const routes: Routes = [
   {
     path: 'main',
     component: MainPageComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ]
+  },
+  {
+    path: 'dash',
+    component: DashboardComponent,
   }
 ];
 
